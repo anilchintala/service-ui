5fb46a980bb4d8b4b8d82bf4b5990deb6070af3b
Maxim Tumas <DiscoElevator@users.noreply.github.com>
2018-06-07 16:45:39 +0300

    EPMRPP-33145: Add redux logic to the filters page

--- app/src/controllers/user/selectors.js
+++ app/src/controllers/user/selectors.js
@@ -17,4 +17,3 @@ export const activeProjectRoleSelector = (state) => {
   return assignedProject && assignedProject.projectRole;
 };
 export const isAdminSelector = (state) => userInfoSelector(state).userRole === ADMINISTRATOR;
-export const userRoleSelector = (state) => userSelector(state).userRole;
